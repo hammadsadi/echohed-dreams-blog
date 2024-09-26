9ce8b8a085b3e097321e76deafdd7f0d1dfe243b
Hammad Sadi <130208279+hammadsadi@users.noreply.github.com>
2024-09-26 10:09:38 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
- DB_URI="mongodb+srv://syedsadi684:ZcUrLxMtQftIrP0u@cluster0.pst1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const API=process.env.NODE_ENV === 'production' ? 'https://xxx:vercel.app/api' : 'http://localhost:3000/api'
+ 
 
